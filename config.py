30941d24c54a9a6bd7da469604a433e334a49a56
isaac mariga <isaacwangombe5@gmail.com>
2022-05-03 17:37:36 +0300

    adding API key and base url

--- config.py
+++ config.py
@@ -5,6 +5,11 @@ class Config:
     '''
     General configuration parent class
     '''
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&apiKey={}'
+
+    ARTICLES_API_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
+
+    NEWS_API_KEY = ('6ad1799356aa4cfd8a50aae815150add')
 
 
 class ProdConfig(Config):
