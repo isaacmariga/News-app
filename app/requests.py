3856d7efff691d1a149ba6647b7247a71ee04d1b
isaac mariga <isaacwangombe5@gmail.com>
2022-05-03 18:04:37 +0300

    requesting sources

--- app/requests.py
+++ app/requests.py
@@ -15,3 +15,49 @@ def configure_request(app):
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_API_BASE_URL']
     base_article_url = app.config['ARTICLES_API_BASE_URL']
+
+
+def get_sources(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_sources_url = base_url.format(api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        sources_results = None
+
+        if get_sources_response['sources']:
+            sources_results_list = get_sources_response['sources']
+            sources_results = process_sources_results(sources_results_list)
+
+    return sources_results
+
+def process_sources_results(sources_list):
+    """
+    Function  that processes the sources result and transform them to a list of Objects
+
+    Args:
+        sources_list: A list of dictionaries that contain sources details
+
+    Returns :
+        sources_results: A list of source objects
+    """
+    
+    sources_results = []
+    for source_item in sources_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category = source_item.get('category')
+        language = source_item.get('language')
+        country = source_item.get('country')
+
+        # if poster:
+        source_object = Sources(id, name, description, url, category, language, country)
+        sources_results.append(source_object)
+
+    return sources_results
