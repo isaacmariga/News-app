899182f5f5315d65efc4088a895df05e1888fb4a
isaac mariga <isaacwangombe5@gmail.com>
2022-05-03 17:37:14 +0300

    adding config info

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&apiKey={}'
+
+    ARTICLES_API_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
+
+    NEWS_API_KEY = ('6ad1799356aa4cfd8a50aae815150add')
