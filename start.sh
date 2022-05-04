960ea0f3d6cd0cbb8ae9c9301e22835e8db35c90
isaac mariga <isaacwangombe5@gmail.com>
2022-05-04 05:54:13 +0300

    Editing start

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-export NEWS_API_KEY = "6ad1799356aa4cfd8a50aae815150add"
+# export NEWS_API_KEY = "6ad1799356aa4cfd8a50aae815150add"
 
-export SECRET_KEY = "123456"
+# export SECRET_KEY = "123456"
 
 python3.9 manage.py server
